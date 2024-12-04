1c638ee4fd24ee7d3c2a4b4ae219586208d5a36c
R3NG0KU-FLAME <terror-16@mail.ru>
2024-12-04 21:33:03 +0500

    тест

--- main.go
+++ main.go
@@ -6,33 +6,46 @@ type employee struct {
 	person    string
 	hitpoints int
 	armor     int
+	damage    int
 }
 
-func newEmployee(person string, hitpoints, armor int) employee {
+func newEmployee(person string, hitpoints, armor, damage int) employee {
 	return employee{
 		person:    person,
 		hitpoints: hitpoints,
 		armor:     armor,
+		damage:    damage,
 	}
 }
 
-func (e employee) getInfo() string {
-	return fmt.Sprintf("Перс: %s\nЗдоровье: %d\nБроня: %d\n", e.person, e.hitpoints, e.armor)
+func (e employee) getDamage() string {
+	return fmt.Sprintf("Персонаж: %s\nЗдоровье: %d\nБроня: %d\nНаносимый урон: %d\n", e.person, e.hitpoints, e.armor, e.damage)
+
+}
+
+func (e employee) setName(person string) {
+	e.person = person
 
 }
 
 func main() {
-	employee1 := newEmployee("Воин", 100, 100)
-	employee2 := newEmployee("Орк", 100, 100)
 
-	fmt.Printf("%s\n", employee1.getInfo())
-	fmt.Printf("%s\n", employee2.getInfo())
-}
+	employee1 := newEmployee("Воин", 100, 100, 15)
+	employee2 := newEmployee("Скелет", 100, 100, 15)
 
-func handleCommand(command string) string {
-	/*
-		данная функция принимает команду от "пользователя"
-		и наверняка вызывает какой-то другой метод или функцию у "мира" - списка комнат
-	*/
-	return "not implemented"
+	fmt.Println("Выбери героя: \n")
+	fmt.Printf("%s\n", employee1.getDamage())
+	fmt.Printf("%s\n", employee2.getDamage())
+
+	a := new
+
+	switch a(employee1.getDamage()) {
+
+	case a(employee1.getDamage()):
+		fmt.Scan(&employee1.person)
+		fmt.Println("Вы выбрали Воина!\nУдачи в схватке!")
+
+	}
+
+	return
 }
